b37de2c37db18be9f26ee8d0e74bb3494674bc20
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-04 17:10:31 -0600

    final version working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,34 +4,71 @@ var letters = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 window.onload = function() {
 
      // 1. computer chooses a letter ->DONE
-    var compChoice = Math.floor(Math.random() * 26); // Line ->generates a random num
-    compChoice = letters[compChoice]; // Line -> picks a letter using the random num as an index value.
-    document.getElementById("guessedRemaining").textContent = ("Guesses Left: " + guessesRemaining);
+    // Line ->generates a random num
+    var compChoice = letters[Math.floor(Math.random() * letters.length) ]; // Line -> picks a letter using the random num as an index value.
+    console.log(compChoice);
+ 
     // 2. user chooses a letter
     var userGuess; // Line stores the onkey input from the user
-// Below -> camputures the user input adds it to both the .guessed html tag and to the userGuess var.
-document.onkeyup = function () {
-        var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-        userGuess = userInput;
-        $(".guessed").append(userInput);
-    };
-
-
+    // Below -> camputures the user input adds it to both the .guessed html tag and to the userGuess var.
     var wins = 0; // Line -> acc wins
     var losses = 0;// Line -> acc  losses
     var guessesRemaining = 10; // Line -> acc chances
-  
-if (userGuess === compChoice) {
-    var wins = 0;
-    wins++;
-    document.getElementById("win").textContent = ("Wins: " + wins);
-} else if (userGuess !== compChoice) {
-    guessesRemaining--;
+    var userLetters = []; 
+
     document.getElementById("guessedRemaining").textContent = ("Guesses Left: " + guessesRemaining);
-} else if (guessesRemaining === 0) {
-    var losses = 0;
-    losses++;
-}
+
+
+    var reset = function () { 
+        userLetters = [];
+        guessesRemaining = 10;
+        compChoice = letters[Math.floor(Math.random() * letters.length)]; 
+        $(".guessed").html("Perevious Guesses: " );
+        $("#guessedRemaining").text("Guesses Left: " + guessesRemaining);
+        console.log(compChoice);
+        console.log(guessesRemaining);
+        //need to update - guessed left prev gueses
+    }
+   
+    document.onkeyup = function (event) {
+        event.stopPropagation();
+        var userInput = event.key.toLowerCase();
+        userGuess = userInput;
+    
+    $('.btn').on("click", function () {
+      alert("You're a winner in my eyes!");
+    });
+
+        if (userLetters.indexOf(userInput) !== -1 || letters.indexOf(userInput) === -1) {
+                return;
+        }
+        userLetters.push(userGuess);
+        $(".guessed").append(userInput);
+        console.log(guessesRemaining);
+
+        if (userGuess === compChoice) {
+            var wins = 0;
+            wins++;
+            document.getElementById("win").textContent = ("Wins: " + wins);
+            reset();
+        } else {
+            guessesRemaining--;
+            $("#guessedRemaining").text("Guesses Left: " + guessesRemaining);
+            if (guessesRemaining === 0) {
+                losses++; 
+                $("#losses").text("Losses: " + losses);
+                reset();
+            }
+        } //else if (guessesRemaining === 0) {
+        //     var losses = 0;
+        //     losses++;
+        // }
+    };
+
+
+ 
+
+
 
     // 3.  if the (user guess === computor )
     //        wins++
